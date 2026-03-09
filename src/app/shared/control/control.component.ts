e2c8fef25a4fe08b2b38928701b6461604b911be
Joemol Joy <joemoljoy.a@gmail.com>
2026-03-09 10:43:47 -0400

    Added host and remove unncessary wrapper

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host:{
+    class: 'control'
+  }// we could have added the class in html file but this way we can ensure that the class is always added to the host element or to the file where we use the component
 })
 export class ControlComponent {
   label = input.required<string>();
